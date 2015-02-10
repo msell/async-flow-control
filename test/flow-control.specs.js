af6f946be1250d1922ec48bf97565b7463e2189c
Matt Sell <mwsell@gmail.com>
2015-02-09 22:29:53 -0600

    add some unimplemented specs

---
+++ test/flow-control.specs.js
@@ -0,0 +1,5 @@
+describe('Adding songbook to api',function(){
+    it('should add all the artists');
+    it('should add all the songs');
+    it('should add the first artists songs before moving on the the second artist');
+})
