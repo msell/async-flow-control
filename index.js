4b46d320efebaa67dd26f1388f65a339d491fc59
Matt Sell <mwsell@gmail.com>
2015-02-10 20:32:04 -0600

    getting close

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var fs = require('fs');
 var async = require('async');
 
+var artistIds = [1,2,3];
+
 var addArtist = function(artist, callback){
+    var artistId;
     setTimeout(function(){
         console.log('added %s', artist.name);
-        callback(null);
+        artistId = artistIds.pop();
     }, 500);
+    
+    async.series(artist.songs.map(function(song){
+        return function(cb){addSong(song, artistId, cb)}
+    }))
+    
+    callback(null);
 }
 
 var addSong = function(name,artistId, callback){
@@ -17,7 +26,7 @@ var addSong = function(name,artistId, callback){
 
 var artists = JSON.parse(fs.readFileSync('./songbook.json','utf-8'));
 
-async.waterfall(artists.map(function(artist){
+async.series(artists.map(function(artist){
     return function(cb){addArtist(artist,cb)}
 })
 );
